bc81d169954668c1632279981774d230d428843d
Amar Chauhan <1706015@kiit.ac.in>
2025-06-19 10:12:39 +0000

    Added decorator pattern

---
+++ Decorator/DecoratorDesignPattern.java
@@ -0,0 +1,109 @@
+package Decorator;
+
+interface Pizza
+{
+    int Cost();
+    String description();
+    void display();
+}
+
+class BasePizza implements Pizza
+{
+
+    @Override
+    public int Cost() {
+        return 10;
+    }
+
+    @Override
+    public void display() {
+        System.err.println(description());
+    }
+
+    @Override
+    public String description() {
+       return "Base pizza";
+    }
+    
+}
+
+abstract class PizzaToppings implements Pizza
+{
+    protected Pizza pizza;
+
+    public PizzaToppings(Pizza pizza) {
+        this.pizza = pizza;
+    }
+    
+    public void display() {
+        System.out.print(description());
+    }
+
+    @Override
+    public int Cost() {
+        return pizza.Cost()+20;
+    }
+
+    @Override
+    public String description() {
+       return pizza.description()+" with cheese topping";
+    }
+}
+
+class CheeseToppings extends PizzaToppings
+{
+    CheeseToppings(Pizza p)
+    {
+        super(p);
+    }
+
+    @Override
+    public void display() {
+        System.err.print(description());
+    }
+
+    @Override
+    public int Cost() {
+        return pizza.Cost()+20;
+    }
+
+    @Override
+    public String description() {
+       return pizza.description()+" with cheese topping";
+    }
+    
+}
+
+class VeggiesTopping extends PizzaToppings
+{
+    VeggiesTopping(Pizza p)
+    {
+        super(p);
+    }
+
+    @Override
+    public void display() {
+        System.err.print(description());
+    }
+
+    @Override
+    public int Cost() {
+        return pizza.Cost()+20;
+    }
+
+    @Override
+    public String description() {
+        return pizza.description()+" with veggies topping";
+    }
+    
+}
+public class DecoratorDesignPattern {
+
+    public static void main(String[] args) {
+        Pizza pizza=new BasePizza();
+        pizza=new VeggiesTopping(pizza);
+        pizza =new CheeseToppings(pizza);    
+        System.out.println(pizza.Cost());
+        pizza.display();
+    }
+}
